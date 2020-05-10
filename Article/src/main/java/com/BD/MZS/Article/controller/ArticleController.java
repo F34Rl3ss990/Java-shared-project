da2aa35422ef784b38f2e98c378fab1e564bfeef
Becze Dániel <f34l3ssqq990@gmail.com>
2020-05-10 20:23:51 +0200

    a

--- Article/src/main/java/com/BD/MZS/Article/controller/ArticleController.java
+++ Article/src/main/java/com/BD/MZS/Article/controller/ArticleController.java
@@ -1,10 +1,15 @@
 package com.BD.MZS.Article.controller;
 
+import com.BD.MZS.Article.controller.dto.ArticleDTO;
 import com.BD.MZS.Article.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.Valid;
 
 @Controller
 public class ArticleController {
@@ -22,4 +27,10 @@ public class ArticleController {
         mav.setViewName("MainArticle");
         return mav;
     }
+/*
+    @PostMapping(value ="/")
+    public ModelAndView AddArticle(@Valid ArticleDTO articleDTO,
+                                   BindingResult bindingResult){
+
+    }*/
 }
