0b4f493c29a71de53001b130c1ede1667111cb01
doxxapapa <36539192+doxxapapa@users.noreply.github.com>
2020-05-10 21:31:14 +0200

    Listing stuff with a simple HTML Table

--- Article/src/main/java/com/BD/MZS/Article/controller/ArticleController.java
+++ Article/src/main/java/com/BD/MZS/Article/controller/ArticleController.java
@@ -4,8 +4,11 @@ import com.BD.MZS.Article.controller.dto.ArticleDTO;
 import com.BD.MZS.Article.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.validation.BindingResult;
 
@@ -21,10 +24,19 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
+
     @GetMapping(value = "/")
     public ModelAndView getRoot() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("MainArticle");
+        return mav;
+    }
+
+    @GetMapping(value = "/GetArticle")
+    public ModelAndView getListed(@RequestParam(value="age", required=false, defaultValue="5") String age, Model model){
+        ModelAndView mav = new ModelAndView();
+        model.addAttribute("age", age);
+        mav.setViewName("GetArticle");
         return mav;
     }
 /*
